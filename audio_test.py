7bdac822a873486cc39f2a43c87a736632478da5
Ola L <ola.lansman@relexsolutions.com>
2018-10-25 16:03:11 +0300

    added ability to play the recording

--- audio_test.py
+++
@@ -1,7 +0,0 @@
-from recorder import Recorder
-
-res = raw_input('Please press Enter when you are ready to start recording')
-
-rec = Recorder(channels=2)
-with rec.open('audio_file_1.wav', 'wb') as recfile:
-    recfile.record(duration=5.0)
